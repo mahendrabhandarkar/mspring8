4a8a7323b8dc14c756c77ac037a3f5b3a1fcf4e6
Mahendra Bhandarkar <mahendrabhandarkar@gmail.com>
2024-02-06 07:39:42 +0530

    SSL

---
+++ src/main/java/com/ks/mspring8/Mspring8Application.java
@@ -0,0 +1,13 @@
+package com.ks.mspring8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mspring8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mspring8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ks/mspring8/Mspring8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.mspring8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mspring8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
