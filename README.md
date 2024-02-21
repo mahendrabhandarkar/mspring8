98a6d8d000e353e1c43cbdea5bb4ea9782a4bfe6
Mahendra Bhandarkar <mahendrabhandarkar@gmail.com>
2024-02-22 04:01:19 +0530

    Added Cache, Spring AI, H2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# mspring8" 
+
+https://github.com/Basir-PD/100-Projects-HTML-CSS-JavaScript
--- src/main/java/com/ks/mspring8/Mspring8Application.java
+++ src/main/java/com/ks/mspring8/Mspring8Application.java
@@ -3,6 +3,13 @@ package com.ks.mspring8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Add annotation @Bean in CorsFilter, passwordEncoder, authenticationManager, authenticationProvider
+ * Create Entity with Serializable
+ *  JIO Brain API Integration with Spring AI
+ *  How to integrate OpenAI Into a Spring Boot Application Using Spring AI
+ */
+
 @SpringBootApplication
 public class Mspring8Application {
 
